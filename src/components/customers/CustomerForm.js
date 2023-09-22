b5ac57df0fb72cc0a6eec8b76908407675120de7
wickeddd3 <pablophilipandrew@gmail.com>
2023-09-22 13:24:10 +0800

    use form validation on CustomerForm component

--- src/components/customers/CustomerForm.js
+++ src/components/customers/CustomerForm.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { createCustomer } from '../../store/reducers/customers';
+import { FormProvider, useForm } from 'react-hook-form';
+import { nameValidation, emailValidation, contactValidation } from '../../utils/validation/form/customerFormValidation';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import DrawerContent from '../common/DrawerContent';
@@ -8,23 +9,13 @@ import FormTextField from '../common/form/FormTextField';
 
 export default function CustomerForm({ close }) {
   const dispatch = useDispatch();
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [contact, setContact] = useState('');
+  const methods = useForm();
 
-  const handleSetName = (value) => setName(value);
-  const handleSetEmail = (value) => setEmail(value);
-  const handleSetContact = (value) => setContact(value);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const form = {
-      name,
-      email,
-      contact,
-    };
-    dispatch(createCustomer(form));
-  };
+  const handleSubmit = methods.handleSubmit((data) => {
+    dispatch(createCustomer(data));
+    methods.reset();
+    close();
+  });
 
   return (
     <DrawerContent
@@ -33,56 +24,37 @@ export default function CustomerForm({ close }) {
       close={close}
       submit={handleSubmit}
     >
-      <Box
-        component="form"
-        noValidate
-        sx={{ mt: 2 }}
-      >
-        <Grid
-          container
-          spacing={2}
+      <FormProvider {...methods}>
+        <Box
+          component="form"
+          noValidate
+          sx={{ mt: 2 }}
         >
           <Grid
-            item
-            xs={12}
+            container
+            spacing={2}
           >
-            <FormTextField
-              id="customerName"
-              name="customerName"
-              label="Customer Name"
-              autoComplete="customerName"
-              value={name}
-              setValue={handleSetName}
-            />
+            <Grid
+              item
+              xs={12}
+            >
+              <FormTextField {...nameValidation} />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+            >
+              <FormTextField {...emailValidation} />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+            >
+              <FormTextField {...contactValidation} />
+            </Grid>
           </Grid>
-          <Grid
-            item
-            xs={12}
-          >
-            <FormTextField
-              id="email"
-              name="email"
-              label="Email"
-              autoComplete="email"
-              value={email}
-              setValue={handleSetEmail}
-            />
-          </Grid>
-          <Grid
-            item
-            xs={12}
-          >
-            <FormTextField
-              id="contact"
-              name="contact"
-              label="Contact"
-              autoComplete="contact"
-              value={contact}
-              setValue={handleSetContact}
-            />
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </FormProvider>
     </DrawerContent>
   );
 }
