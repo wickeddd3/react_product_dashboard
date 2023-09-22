f9a3171c8239630d601ce71cb70dd7933256300c
wickeddd3 <pablophilipandrew@gmail.com>
2023-09-22 13:23:41 +0800

    add validation and error message on FormTextField component

--- src/components/common/form/FormTextField.js
+++ src/components/common/form/FormTextField.js
@@ -1,4 +1,8 @@
+import { useFormContext } from 'react-hook-form';
+import { AnimatePresence } from 'framer-motion';
+import { findInputError, isFormInvalid } from '../../../utils/validation/index';
 import TextField from '@mui/material/TextField';
+import FormInputError from './FormInputError';
 
 export default function FormTextField(props) {
   const {
@@ -7,20 +11,44 @@ export default function FormTextField(props) {
     label = '',
     autoComplete = 'textField',
     size = 'small',
+    validation,
     value,
     setValue,
   } = props;
+
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const inputErrors = findInputError(errors, name);
+  const isInvalid = isFormInvalid(inputErrors);
+
   return (
-    <TextField
-      required
-      fullWidth
-      id={id}
-      label={label}
-      name={name}
-      autoComplete={autoComplete}
-      size={size}
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-    />
+    <>
+      <TextField
+        fullWidth
+        id={id}
+        label={label}
+        name={name}
+        autoComplete={autoComplete}
+        size={size}
+        value={value}
+        error={isInvalid}
+        onChange={(e) => setValue(e.target.value)}
+        {...register(name, validation)}
+      />
+      <AnimatePresence
+        mode="wait"
+        initial={false}
+      >
+        {isInvalid && (
+          <FormInputError
+            message={inputErrors.error.message}
+            key={inputErrors.error.message}
+          />
+        )}
+      </AnimatePresence>
+    </>
   );
 }
