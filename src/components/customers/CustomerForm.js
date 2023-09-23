eecd38afc1cb9bdbf2c72452b681155a9740a3c7
wickeddd3 <pablophilipandrew@gmail.com>
2023-09-23 17:15:32 +0800

    handle update customer

--- src/components/customers/CustomerForm.js
+++ src/components/customers/CustomerForm.js
@@ -1,5 +1,6 @@
-import { useDispatch } from 'react-redux';
-import { createCustomer } from '../../store/reducers/customers';
+import { useMemo } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { createCustomer, updateCustomer } from '../../store/reducers/customers';
 import { FormProvider, useForm } from 'react-hook-form';
 import { nameValidation, emailValidation, contactValidation } from '../../utils/validation/form/customerFormValidation';
 import Grid from '@mui/material/Grid';
@@ -8,19 +9,28 @@ import DrawerContent from '../common/DrawerContent';
 import FormTextField from '../common/form/FormTextField';
 
 export default function CustomerForm({ close }) {
+  const selectedCustomer = useSelector((state) => state.customers.selectedCustomer);
   const dispatch = useDispatch();
-  const methods = useForm();
+  const methods = useForm({ defaultValues: selectedCustomer });
 
   const handleSubmit = methods.handleSubmit((data) => {
-    dispatch(createCustomer(data));
+    selectedCustomer?.id ? dispatch(updateCustomer(data)) : dispatch(createCustomer(data));
     methods.reset();
     close();
   });
 
+  const drawerTitle = useMemo(() => {
+    return selectedCustomer?.id ? 'Edit customer' : 'Create new customer';
+  }, [selectedCustomer]);
+
+  const submitButtonText = useMemo(() => {
+    return selectedCustomer?.id ? 'Update' : 'Create';
+  }, [selectedCustomer]);
+
   return (
     <DrawerContent
-      title="Create new customer"
-      buttonText="Create"
+      title={drawerTitle}
+      buttonText={submitButtonText}
       close={close}
       submit={handleSubmit}
     >
