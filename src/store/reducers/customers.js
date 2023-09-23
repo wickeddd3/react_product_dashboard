4b2592a2d543657bd549d8bf86b5fae1369e306d
wickeddd3 <pablophilipandrew@gmail.com>
2023-09-23 17:14:53 +0800

    add selectCustomer, resetSelectedCustomer, and updateCustomer

--- src/store/reducers/customers.js
+++ src/store/reducers/customers.js
@@ -20,34 +20,46 @@ const initialState = {
       disablePadding: true,
       label: 'Contact',
     },
+    {
+      id: 'actions',
+      numeric: false,
+      disablePadding: true,
+      label: '',
+    },
   ],
   rows: [
     {
+      id: 1,
       name: 'John Doe',
       email: 'johndoe@gmail.com',
       contact: '2234-2342-0000',
     },
     {
+      id: 2,
       name: 'Tony Stark',
       email: 'tonystark@gmail.com',
       contact: '2234-4444-0000',
     },
     {
+      id: 3,
       name: 'Bruce Banner',
       email: 'brucebanner@gmail.com',
       contact: '2234-7878-0000',
     },
     {
+      id: 4,
       name: 'Peter Parker',
       email: 'peterparker@gmail.com',
       contact: '2234-2222-0000',
     },
     {
+      id: 5,
       name: 'Bruce Wayne',
       email: 'brucewayne@gmail.com',
       contact: '2234-4662-0000',
     },
   ],
+  selectedCustomer: {},
 };
 
 export const customersSlice = createSlice({
@@ -56,12 +68,25 @@ export const customersSlice = createSlice({
   reducers: {
     createCustomer: (state, action) => {
       const rows = [...state.rows];
-      rows.push(action.payload);
+      rows.push({ ...action.payload, id: state.rows.length + 1 });
+      state.rows = rows;
+    },
+    selectCustomer: (state, action) => {
+      state.selectedCustomer = action.payload;
+    },
+    resetSelectedCustomer: (state) => {
+      state.selectedCustomer = {};
+    },
+    updateCustomer: (state, action) => {
+      const rows = [...state.rows];
+      const updatedCustomer = action.payload;
+      const index = rows.findIndex((row) => row.id === updatedCustomer.id);
+      rows[index] = updatedCustomer;
       state.rows = rows;
     },
   },
 });
 
-export const { createCustomer } = customersSlice.actions;
+export const { createCustomer, selectCustomer, resetSelectedCustomer, updateCustomer } = customersSlice.actions;
 
 export default customersSlice.reducer;
