488246d43dfa6ad8ca1bf3bcf2f7d6db4ef4178c
wickeddd3 <pablophilipandrew@gmail.com>
2023-09-24 17:11:21 +0800

    use DialogProvider and Dialog on main App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import './App.css';
+import DialogProvider from './providers/DialogProvider';
+import Dialog from './components/common/Dialog';
 
 function App() {
   return (
     <div className="app">
-      <RouterProvider router={router} />
+      <DialogProvider>
+        <RouterProvider router={router} />
+        <Dialog />
+      </DialogProvider>
     </div>
   );
 }
