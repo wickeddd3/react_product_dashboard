eb8b8520199e276c3a0354f3acf4135b78dd7bf1
wickeddd3 <pablophilipandrew@gmail.com>
2023-09-25 13:15:43 +0800

    delete drawer store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import drawerReducer from './reducers/drawer';
 import customersReducer from './reducers/customers';
 
 export default configureStore({
   reducer: {
-    drawer: drawerReducer,
     customers: customersReducer,
   },
 });
