bf01697d058cb06937dc182eefbc5d85ae0bf717
wickeddd3 <pablophilipandrew@gmail.com>
2023-09-25 12:44:16 +0800

    use DrawerContext on Drawer component

--- src/components/common/Drawer.js
+++ src/components/common/Drawer.js
@@ -1,23 +1,35 @@
-import * as React from 'react';
+import { useContext, useCallback } from 'react';
+import DrawerContext from '../../contexts/DrawerContext';
 import MuiDrawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
 import DynamicComponent from './DynamicComponent';
 
-export default function Drawer({ show, component, anchor = 'right', closeDrawer }) {
+export default function Drawer({ children, ...rest }) {
+  const { drawer, setDrawer } = useContext(DrawerContext);
+  const { show, component, anchor } = drawer;
+
+  const resetDrawer = useCallback(() => {
+    setDrawer({ show: false, component: null, anchor: 'right' });
+  }, [setDrawer]);
+
+  if (!show) return null;
+
   return (
     <div>
       <MuiDrawer
+        {...rest}
         anchor={anchor}
         open={show}
-        onClose={() => closeDrawer(false)}
+        onClose={() => resetDrawer()}
       >
+        {children}
         <Box
           sx={{ width: 350 }}
           role="presentation"
         >
           <DynamicComponent
             is={component}
-            close={closeDrawer}
+            close={resetDrawer}
           />
         </Box>
       </MuiDrawer>
