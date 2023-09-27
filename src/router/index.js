4e2a2a1bd8357be18aed2a6bcae10718061fad04
wickeddd3 <pablophilipandrew@gmail.com>
2023-09-27 12:14:32 +0800

    update new product pathname

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
         element: <Products />,
       },
       {
-        path: '/products/new',
+        path: '/products/new-product',
         element: <NewProduct />,
       },
     ],
