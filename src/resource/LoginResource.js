beceee09b0bd78f0a6e53121b68ea1dbb9292751
wickeddd3 <pablophilipandrew@gmail.com>
2023-10-06 17:57:33 +0800

    add LoginResource

--- src/resource/LoginResource.js
+++ src/resource/LoginResource.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export default class LoginResource {
-  async login(data) {
+  login(data) {
     const baseUrl = process.env.REACT_APP_BASE_URL;
     return axios({
       method: 'post',
-      url: `${baseUrl}/api/login`,
-      data,
+      url: `${baseUrl}/api/auth/login`,
+      data: data,
     })
       .then((response) => response)
       .catch((error) => error.response);
