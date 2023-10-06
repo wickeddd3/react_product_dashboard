5d0a49a4d37c91dbd8ea22b82cefdaf95885f414
wickeddd3 <pablophilipandrew@gmail.com>
2023-10-06 17:59:34 +0800

    add authReducer on store index

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import customersReducer from './reducers/customers';
 import productsReducer from './reducers/products';
+import authReducer from './reducers/auth';
 
 export default configureStore({
   reducer: {
     customers: customersReducer,
     products: productsReducer,
+    auth: authReducer,
   },
 });
