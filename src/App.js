c2b27a67647bf597907ae3ebb7963a4d7f819d39
wickeddd3 <pablophilipandrew@gmail.com>
2023-10-08 19:12:28 +0800

    add Router on App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
+import Router from './router';
 import './App.css';
 import DrawerProvider from './providers/DrawerProvider';
 import Drawer from './components/common/drawer/Drawer';
 import DialogProvider from './providers/DialogProvider';
 import Dialog from './components/common/Dialog';
+import { AuthProvider } from './providers/AuthProvider';
 import { SnackbarProvider } from 'notistack';
 
 function App() {
   return (
     <div className="app">
-      <DrawerProvider>
-        <DialogProvider>
-          <SnackbarProvider
-            maxSnack={5}
-            autoHideDuration={3000}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            dense
-          >
-            <RouterProvider router={router} />
-            <Drawer />
-            <Dialog />
-          </SnackbarProvider>
-        </DialogProvider>
-      </DrawerProvider>
+      <AuthProvider>
+        <DrawerProvider>
+          <DialogProvider>
+            <SnackbarProvider
+              maxSnack={5}
+              autoHideDuration={3000}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              dense
+            >
+              <Router />
+              <Drawer />
+              <Dialog />
+            </SnackbarProvider>
+          </DialogProvider>
+        </DrawerProvider>
+      </AuthProvider>
     </div>
   );
 }
