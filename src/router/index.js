03ffabbd46debf930dd347753e43ba9cfb76d05b
wickeddd3 <pablophilipandrew@gmail.com>
2023-10-08 19:11:21 +0800

    rewrite router.js to support router guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,65 @@
-import { createBrowserRouter } from 'react-router-dom';
-import DefaultLayout from '../components/layouts/DefaultLayout.js';
-import Dashboard from '../pages/Dashboard.js';
-import Customers from '../pages/Customers.js';
-import Products from '../pages/Products.js';
-import NewProduct from '../pages/NewProduct.js';
-import Login from '../pages/Login.js';
+import { Routes, Route } from 'react-router-dom';
+import AuthGuard from './guards/AuthGuard';
+import GuestGuard from './guards/GuestGuard';
+import Dashboard from '../pages/Dashboard';
+import Customers from '../pages/Customers';
+import Products from '../pages/Products';
+import NewProduct from '../pages/NewProduct';
+import Login from '../pages/Login';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <DefaultLayout />,
-    children: [
-      {
-        path: '/',
-        element: <Dashboard />,
-      },
-      {
-        path: '/customers',
-        element: <Customers />,
-      },
-      {
-        path: '/products',
-        element: <Products />,
-      },
-      {
-        path: '/products/new-product',
-        element: <NewProduct />,
-      },
-    ],
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-]);
+const router = () => {
+  return (
+    <Routes>
+      <Route
+        path="/login"
+        element={
+          <GuestGuard>
+            <Login />
+          </GuestGuard>
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <AuthGuard>
+            <Dashboard />
+          </AuthGuard>
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          <AuthGuard>
+            <Dashboard />
+          </AuthGuard>
+        }
+      />
+      <Route
+        path="/customers"
+        element={
+          <AuthGuard>
+            <Customers />
+          </AuthGuard>
+        }
+      />
+      <Route
+        path="/products"
+        element={
+          <AuthGuard>
+            <Products />
+          </AuthGuard>
+        }
+      />
+      <Route
+        path="/products/new-product"
+        element={
+          <AuthGuard>
+            <NewProduct />
+          </AuthGuard>
+        }
+      />
+    </Routes>
+  );
+};
 
 export default router;
