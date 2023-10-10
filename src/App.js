2fe4c0947e0d518fd32c3cc37d69e61f2ffd1f8a
wickeddd3 <pablophilipandrew@gmail.com>
2023-10-10 12:10:02 +0800

    remove auth context and provider

--- src/App.js
+++ src/App.js
@@ -4,31 +4,28 @@ import DrawerProvider from './providers/DrawerProvider';
 import Drawer from './components/common/drawer/Drawer';
 import DialogProvider from './providers/DialogProvider';
 import Dialog from './components/common/Dialog';
-import { AuthProvider } from './providers/AuthProvider';
 import { SnackbarProvider } from 'notistack';
 
 function App() {
   return (
     <div className="app">
-      <AuthProvider>
-        <DrawerProvider>
-          <DialogProvider>
-            <SnackbarProvider
-              maxSnack={5}
-              autoHideDuration={3000}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              dense
-            >
-              <Router />
-              <Drawer />
-              <Dialog />
-            </SnackbarProvider>
-          </DialogProvider>
-        </DrawerProvider>
-      </AuthProvider>
+      <DrawerProvider>
+        <DialogProvider>
+          <SnackbarProvider
+            maxSnack={5}
+            autoHideDuration={3000}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            dense
+          >
+            <Router />
+            <Drawer />
+            <Dialog />
+          </SnackbarProvider>
+        </DialogProvider>
+      </DrawerProvider>
     </div>
   );
 }
