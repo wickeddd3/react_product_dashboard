25d442196c9600f42b5af8054d2b32c035cf26dd
wickeddd3 <pablophilipandrew@gmail.com>
2023-10-11 23:58:35 +0800

    add SubMenu

---
+++ src/components/partials/Sidebar/SubMenu.js
@@ -0,0 +1,38 @@
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import List from '@mui/material/List';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import MenuItem from './MenuItem';
+
+export default function SubMenu({ item, open, drawerState, handleClick }) {
+  return (
+    <>
+      <ListItemButton onClick={handleClick}>
+        <ListItemIcon>{item.icon}</ListItemIcon>
+        <ListItemText primary={item.title} />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse
+        in={open}
+        timeout="auto"
+        unmountOnExit
+      >
+        <List
+          component="div"
+          disablePadding
+        >
+          {item.subNav.map((nav, index) => (
+            <MenuItem
+              item={nav}
+              key={index}
+              sx={drawerState ? { pl: 4 } : null}
+            />
+          ))}
+        </List>
+      </Collapse>
+    </>
+  );
+}
